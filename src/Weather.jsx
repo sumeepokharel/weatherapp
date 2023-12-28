c94e1da9eaea84f4538b2aabb29fb4aba13ea159
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2023-12-28 09:48:05 -0600

    weather app

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,54 +1,86 @@
 import React, { useState } from "react";
-import axios from "axios";
 import styles from "./weather.module.css";
 
-const Weather = () => {
+function App() {
   const [city, setCity] = useState("");
-  const [temperature, setTemperature] = useState("");
-  const [weather, setWeather] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
 
   const apiKey = "440cadb1a012a62858556aaff5c56c47";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather`;
 
-  const fetchWeatherData = async () => {
+  const getWeather = async () => {
     try {
-      const response = await axios.get(apiUrl, {
-        params: {
-          q: city,
-          appid: apiKey,
-          units: "metric",
-        },
-      });
+      const response = await fetch(
+        `${apiUrl}?q=${city}&appid=${apiKey}&units=imperial`
+      );
 
-      const { main, weather: weatherInfo } = response.data;
-      setTemperature(main.temp);
-      setWeather(weatherInfo[0].description);
+      const data = await response.json();
+      setWeatherData(data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
   };
 
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getWeather();
+  };
+
   return (
-    <div className={styles.weatherContainer}>
-      <h1>Weather App</h1>
-      <div className={styles.inputContainer}>
+    <>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter city name"
           value={city}
-          onChange={(e) => setCity(e.target.value)}
-          className={styles.inputField}
+          onChange={handleCityChange}
         />
-        <button onClick={fetchWeatherData}>Get Weather</button>
-      </div>
-      {temperature && weather && (
-        <div className={styles.resultContainer}>
-          <p>Temperature: {temperature}°C</p>
-          <p>Weather: {weather}</p>
+        <button type="submit">Get Weather</button>
+      </form>
+
+      {weatherData && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{weatherData.name}</p>
+            </div>
+            <div className="temp">
+              {weatherData.main ? <h1>{weatherData.main.temp}°F</h1> : null}
+            </div>
+            <div className="description">
+              {weatherData.weather ? (
+                <p>{weatherData.weather[0].main}</p>
+              ) : null}
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              {weatherData.main ? (
+                <p className="bold">{weatherData.main.feels_like}°F</p>
+              ) : null}
+            </div>
+            <div className="humidity">
+              {weatherData.main ? (
+                <p className="bold">{weatherData.main.humidity}%</p>
+              ) : null}
+            </div>
+            <div className="wind">
+              {weatherData.wind ? (
+                <>
+                  <p className="bold">{weatherData.wind.speed}MPH</p>
+                  <p>Wind Speed</p>
+                </>
+              ) : null}
+            </div>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
-};
+}
 
-export default Weather;
+export default App;
